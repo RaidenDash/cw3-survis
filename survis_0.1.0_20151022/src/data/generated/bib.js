bdd213bd5776e2da4286bbb38dc031978d5e7bc8
RaidenDash <128918614+RaidenDash@users.noreply.github.com>
2025-05-09 17:18:46 +0100

    Add files via upload

---
+++ survis_0.1.0_20151022/src/data/generated/bib.js
@@ -0,0 +1,92 @@
+﻿define({ entries : {
+    "bhattarai2023": {
+        "author": "Kritib Bhattarai and Sivaraman Rajaganapathy and Trisha Das and Yejin Kim and Yongbin Chen and Nansu Zong",
+        "doi": "10.1093/jamiaopen/ocad135",
+        "journal": "JAMIA Open",
+        "keywords": "Reinforcement Learning, MMSE, EHR, Treatment, Q-learning, Policy Iteration",
+        "title": "Using artificial intelligence to learn optimal regimen plan for Alzheimer\u2019s disease",
+        "type": "article",
+        "year": "2023"
+    },
+    "elsappagh2021": {
+        "author": "Shaker El-Sappagh and Jose M. Alonso and S. M. Riazul Islam and Ahmad M. Sultan and Kyung Sup Kwak",
+        "doi": "10.1038/s41598-021-82098-3",
+        "journal": "Scientific Reports",
+        "keywords": "Random Forest, XAI, Multimodal, SHAP, Fuzzy, Diagnosis, Progression",
+        "title": "A multilayer multimodal detection and prediction model based on explainable artificial intelligence for Alzheimer\u2019s disease",
+        "type": "article",
+        "year": "2021"
+    },
+    "gao2023": {
+        "author": "Jingjing Gao and Jiaxin Liu and Yuhang Xu and Dawei Peng and Zhengning Wang",
+        "doi": "10.3389/fnins.2023.1222751",
+        "journal": "Frontiers in Neuroscience",
+        "keywords": "GNN, fMRI, Brain Age, Graph Neural Network, Prediction",
+        "title": "Brain age prediction using the graph neural network based on resting-state functional MRI in Alzheimer\u2019s disease",
+        "type": "article",
+        "year": "2023"
+    },
+    "grueso2021": {
+        "author": "Sergio Grueso and Raquel Viejo-Sobera",
+        "doi": "10.1186/s13195-021-00900-w",
+        "journal": "Alzheimer\u2019s Research & Therapy",
+        "keywords": "SVM, CNN, MRI, PET, MCI, Progression, Review",
+        "title": "Machine learning methods for predicting progression from mild cognitive impairment to Alzheimer\u2019s disease dementia: a systematic review",
+        "type": "article",
+        "year": "2021"
+    },
+    "jo2019": {
+        "author": "Taeho Jo and Kewei Chen and Ying Liu and Eric Reiman and Yalin Wang",
+        "doi": "10.3389/fnagi.2019.00220",
+        "journal": "Frontiers in Aging Neuroscience",
+        "keywords": "CNN, Autoencoder, Multimodal, MRI, PET, MCI, Classification",
+        "title": "Deep Learning in Alzheimer\u2019s Disease: Diagnostic Classification and Prognostic Prediction Using Neuroimaging Data",
+        "type": "article",
+        "year": "2019"
+    },
+    "kale2024": {
+        "author": "Mayur Kale and Nitu Wankhede and Rupali Pawar and Suhas Ballal and Rohit Kumawat and Manish Goswami and Mohammad Khalid and Brijesh Taksande and Aman Upaganlawar and Milind Umekar and Spandana Rajendra Kopalli and Sushruta Koppula",
+        "doi": "10.1016/j.arr.2024.102497",
+        "journal": "Ageing Research Reviews",
+        "keywords": "AI System, Early Diagnosis, Prognosis, Multimodal, Treatment, Pipeline",
+        "title": "AI-driven innovations in Alzheimer\u2019s disease: Integrating early diagnosis, personalized treatment, and prognostic modelling",
+        "type": "article",
+        "year": "2024"
+    },
+    "khosroshahi2025": {
+        "author": "Mahdieh Taiyeb Khosroshahi and Soroush Morsali and Sohrab Gharakhanlou and Alireza Motamedi and Saeid Hassanbaghlou and Hadi Vahedi and Siamak Pedrammehr and Hussain Mohammed Dipu Kabir and Ali Jafarizadeh",
+        "doi": "10.3390/diagnostics15050612",
+        "journal": "Diagnostics",
+        "keywords": "XAI, MRI, PET, SHAP, LIME, Interpretability, Review",
+        "title": "Explainable Artificial Intelligence in Neuroimaging of Alzheimer\u2019s Disease",
+        "type": "article",
+        "year": "2025"
+    },
+    "odusami2023": {
+        "author": "Modupe Odusami and Rytis Maskeli\u016bnas and Robertas Dama\u0161evi\u010dius and Sanjay Misra",
+        "doi": "10.1007/s40846-023-00801-3",
+        "journal": "Journal of Medical and Biological Engineering",
+        "keywords": "CNN, PET, MRI, Multimodal, Diagnosis, Explainability",
+        "title": "Explainable Deep-Learning-Based Diagnosis of Alzheimer\u2019s Disease Using Multimodal Input Fusion of PET and MRI Images",
+        "type": "article",
+        "year": "2023"
+    },
+    "topol2019": {
+        "author": "Eric J. Topol",
+        "doi": "10.1038/s41591-018-0300-7",
+        "journal": "Nature Medicine",
+        "keywords": "AI, Overview, Clinical AI, Radiology, Neuroimaging, Strategy",
+        "title": "High-performance medicine: the convergence of human and artificial intelligence",
+        "type": "article",
+        "year": "2019"
+    },
+    "yuan2020": {
+        "author": "Yuan Sh and Benliang Liu and Jianhu Zhang and Ying Zhou and Zhiyuan Hu and Xiuli Zhang",
+        "doi": "10.2605767351",
+        "journal": "Computational and Mathematical Methods in Medicine",
+        "keywords": "Blood Biomarker, Transcriptome, SVM, ANN, Diagnosis, Non-invasive",
+        "title": "Application of Artificial Intelligence Modeling Technology Based on Fluid Biopsy to Diagnose Alzheimer\u2019s Disease",
+        "type": "article",
+        "year": "2020"
+    }
+}});
